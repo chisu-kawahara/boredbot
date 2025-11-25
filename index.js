ccb80c8e61246a23ee23358d33ef94f68e0b15f1
chisu-kawahara <chisu.kawahara@gmail.com>
2025-11-25 11:39:20 -0800

    add styling

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-/**
-Challenge: 
-
-- Add some styling! Be creative, play with layout a bit
-  (Spoiler: I'm going to use a single-column flexbox layout)
-  add some color, grab a font from Google fonts - whatever
-  you'd like!
-*/
 
 // fetch("https://apis.scrimba.com/bored/api/activity")
 //     .then(response => response.json())
@@ -13,3 +5,4 @@ Challenge:
 //         console.log(data)
 //         document.getElementById("activity-name").textContent = data.activity
 //     })
+
